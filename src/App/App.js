9241df1c8bd4de586397439a8acb7c15c783da69
danielsolomon332 <daniel.solomon332@gmail.com>
2022-04-20 13:12:58 -0600

    Refactor state and add nav to render and add imports and begin findPokemon function

--- src/App/App.js
+++ src/App/App.js
@@ -1,27 +1,32 @@
 import React, { Component } from 'react'
-
+// import Main from '../Main/Main'
+import Nav from '../Nav/Nav'
 import './App.css'
 
 export class App extends Component {
   constructor() {
     super()
     this.state = {
-      singlePokemon: {
-        name: '',
-        pokedexNumber: null,
-        bulbapediaLink: null
-      },
+      allPokemon: [],
+      singlePokemon: null,
       genXPokemon: [],
       favoritedPokemon: [],
       errorMsg: '',
     }
   }
 
+  findPokemon = (input) => {
+
+  }
+
   render() {
     return (
       <div>
-        <h1>Welcome to Pokestalgia!</h1>
-        <p>The App for old fans to discover new pokemon and where newer fans can rediscover older pokemon!</p>
+        <Nav />
+        <main>
+          <h1>Welcome to Pokestalgia!</h1>
+          <p>The App for old fans to discover new pokemon and where newer fans can rediscover older pokemon!</p>
+        </main>
       </div>
     )
   }
