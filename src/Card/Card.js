ffd398369d80cdb6c42d494b225a9ed8d8e055d0
danielsolomon332 <daniel.solomon332@gmail.com>
2022-04-21 14:59:14 -0600

    Add Card elements

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -3,6 +3,12 @@ import './Card.css'
 
 const Card = () => {
   <div className="card">
-    
+    <p>Name: {}</p>
+    <p>Pokedex #: {}</p>
+    <p>Bulbapedia Link:</p>
+    <a href=`https:bulbapedia.bulbagarden.net/wiki/${}_(Pok%C3%A9mon)`>
+    <div className='favorite-button'>
+      <button className='favorite-button' onClick={() => saveFavoritePokemon()}>Favorite</button>
+    </div>
   </div>
 }
