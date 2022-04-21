3d4fbdf6a693a8f218221ed034b93da2ecd0fd81
danielsolomon332 <daniel.solomon332@gmail.com>
2022-04-21 17:50:05 -0600

    Add NavLink

--- src/Gen1/Gen1Info/Gen1Info.js
+++ src/Gen1/Gen1Info/Gen1Info.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import './Gen1Info.css'
+import { NavLink } from 'react-router-dom'
 
 const Gen1Info = ({ name, id }) => {
   return (
     <div className="card">
+    <NavLink to='/' >
     <p>Name: {name.charAt(0).toUpperCase() + name.slice(1)}</p>
     <p>Pokedex #: {id}</p>
     <a href={`https:bulbapedia.bulbagarden.net/wiki/${name}_(Pok%C3%A9mon)`} target="_blank" rel="noreferrer">{name.charAt(0).toUpperCase() + name.slice(1)} Info Link</a>
     <div className='favorite-button'>
     <button className='favorite-button'>Favorite</button>
     </div>
+    </NavLink>
     </div>
   )
 }
