6d2effc6f65ff5f4b2e6e6519c8745ddc6faaa44
danielsolomon332 <daniel.solomon332@gmail.com>
2022-04-23 13:30:37 -0600

    Refactor styling and add pokemon starters image

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import './Main.css'
+import pokemon_starters from '../pokemon_starters.jpeg'
 
 const Main = () => {
    return (
-    <div>
-      <p>Welcome to Pokestalgia!</p>
-      <p>The App created older fans to relive their Pokestalgia and learn about new pocket monsters too!</p>
-      <p>This App is also a place for newer fans to rediscover the older Pokemon generations!</p>
+    <div classNmae='main-section'>
+      <div className='welcome-message'>
+        <p>Welcome to Pokestalgia!</p>
+        <p>The App created older fans to relive their Pokestalgia and learn about new Pocket Monsters too!</p>
+        <p>This App is also a place for newer fans to rediscover the older Pokemon generations!</p>
+      </div>
+      <div>
+        <img className='pokemon-starters' src={pokemon_starters} alt="pokemon starters" />
+      </div>
     </div>
  )
 }
