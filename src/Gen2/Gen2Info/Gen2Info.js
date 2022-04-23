efbe92bf829cffd0b5097ae39c7b3ce74b9d5bbd
danielsolomon332 <daniel.solomon332@gmail.com>
2022-04-23 15:49:29 -0600

    Receive addFavorite prop and add onClick function to add favorite

--- src/Gen2/Gen2Info/Gen2Info.js
+++ src/Gen2/Gen2Info/Gen2Info.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import './Gen2Info.css'
 
-const Gen2Info = ({ name, id }) => {
+const Gen2Info = ({ addFavorite, name, id }) => {
   return (
     <div className="card">
       <p className='name'>{name.charAt(0).toUpperCase() + name.slice(1)}</p>
       <p className='pokedex'>Pokedex #: {id}</p>
       <a className='bulbapedia-link' href={`https:bulbapedia.bulbagarden.net/wiki/${name}_(Pok%C3%A9mon)`} target="_blank" rel="noreferrer">{name.charAt(0).toUpperCase() + name.slice(1)} Info Link</a>
       <div className='favorite-button'>
-        <button className='add-favorite'>Favorite</button>
+        <button className='add-favorite' onClick={() => addFavorite(id)}>Favorite</button>
       </div>
     </div>
   )
