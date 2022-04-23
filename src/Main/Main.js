b75e1807f4f51e8547a654d5113573c1e2ba7bf8
danielsolomon332 <daniel.solomon332@gmail.com>
2022-04-23 13:01:24 -0600

    Initial main component build

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,8 +1,14 @@
-// import React from 'react'
-// import './Main.css'
-//
-// const Main = () = {
-//  return ()
-// }
-//
-// export default Main
+import React from 'react'
+import './Main.css'
+
+const Main = () => {
+   return (
+    <div>
+      <p>Welcome to Pokestalgia!</p>
+      <p>The App created older fans to relive their Pokestalgia and learn about new pocket monsters too!</p>
+      <p>This App is also a place for newer fans to rediscover the older Pokemon generations!</p>
+    </div>
+ )
+}
+
+export default Main
